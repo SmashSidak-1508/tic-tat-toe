cebda41abe76e9710394705f3c4c1718d41add37
Sidak2002 <batchit24@gmail.com>
2023-08-22 18:13:46 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 
 import './App.css'
-import Avatar from './avatar'
 import Grid from './components/Grid/Grid'
-import Icon from './components/Icon/Icon'
-import Card from './components/card/Card'
 
 function App() {
 
